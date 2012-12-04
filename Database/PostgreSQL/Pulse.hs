7285216ccf35fb6b1418d6b1a64f588dc4df1fd1
Jason Dusek <jason.dusek@gmail.com>
2012-12-04 16:12:46 +0000

    Works but slow!

--- Database/PostgreSQL/Pulse.hs
+++ Database/PostgreSQL/Pulse.hs
@@ -1,61 +1,128 @@
 #!/usr/bin/env runhaskell
 {-# LANGUAGE OverloadedStrings
            , ScopedTypeVariables
+           , NoMonomorphismRestriction
+           , ViewPatterns
   #-}
 module Database.PostgreSQL.Pulse where
 
 import           Control.Applicative
+import           Control.Arrow (first)
 import           Control.Concurrent
-import           Control.Concurrent.STM (STM)
-import qualified Control.Concurrent.STM as STM
-import qualified Control.Concurrent.STM.TChan as STM
+import           Control.Concurrent.STM
+import           Control.Concurrent.STM.TVar
+import           Control.Concurrent.STM.TChan
 import           Control.Monad
+import           Data.Monoid
+import           Data.Word
 import           System.IO
 
 import           Data.ByteString (ByteString)
 import qualified Data.ByteString as ByteString hiding (hPutStrLn)
-import qualified Data.ByteString.Char8 as ByteString (lines, hPutStrLn)
-import qualified Data.Csv as Cassava
-import qualified Data.Csv.Incremental as Cassava
-import           Data.Vector (Vector)
-import qualified Data.Vector as Vector
+import qualified Data.ByteString.Char8 as ByteString
+import           Data.Either
 
 
 main :: IO ()
-main  = do (a :: STM.TChan ByteString)          <- STM.atomically $ STM.newTChan
-           (b :: STM.TChan (Vector ByteString)) <- STM.atomically $ STM.newTChan
-           (c :: STM.TChan [Vector ByteString]) <- STM.atomically $ STM.newTChan
-           _ <- forkIO . forever $ getBlock a
-           _ <- forkIO . forever . STM.atomically $ breakIntoRecords a b
-           _ <- forkIO . forever $ send c
-           forever $ delayedBulkTransfer 500000 b c
+main  = go 500000 stdin
 
-delayedBulkTransfer :: Int -> STM.TChan t -> STM.TChan [t] -> IO ()
-delayedBulkTransfer micros from to = do
-  (forkIO . STM.atomically) (STM.writeTChan to =<< readAll from)
+go :: Int -> Handle -> IO ()
+go t h = do (blocks, junk, csv, chunks, fuzz) <- context
+            forget . forever $ recv h         blocks
+            forget . forever $ segments fuzz  blocks junk csv
+            forget . forever $ timedMessages t       junk
+            forget . forever $ timedHandoff t             csv chunks
+            forever          $ send                           chunks
+
+context :: IO ( TChan ByteString, TChan ByteString,
+                TChan ByteString, TChan [ByteString], TVar ByteString )
+context  = atomically $ do
+  (,,,,) <$> newTChan <*> newTChan <*> newTChan <*> newTChan <*> newTVar ""
+
+
+recv :: Handle -> TChan ByteString -> IO ()
+recv h o = do bytes <- ByteString.hGetSome h 16384
+              when (bytes /= "") $ do -- msg "Got a block."
+                                      atomically (writeTChan o bytes)
+
+segments :: TVar ByteString
+         -> TChan ByteString -> TChan ByteString -> TChan ByteString -> IO ()
+segments fuzz blocks junk csv = atomically $ do
+  bytes <- readTChan blocks
+  ("" /= bytes) `when` do
+    leftover <- readTVar fuzz
+    let (results, remainder) = segment (leftover <> bytes)
+        (bad, good)          = partitionEithers results
+    mapM_ (writeTChan csv)  good
+    mapM_ (writeTChan junk) bad
+    writeTVar fuzz remainder
+
+timedMessages :: Int -> TChan ByteString -> IO ()
+timedMessages micros junk = do
+  forget (maybeList "Bad CSV:" =<< atomically (readAll junk))
   threadDelay micros
 
-readAll :: STM.TChan t -> STM [t]
-readAll chan = do h <- STM.tryReadTChan chan
+timedHandoff :: Int -> TChan t -> TChan [t] -> IO ()
+timedHandoff micros from to = do
+  forget $ atomically (writeNonEmpty to =<< readAll from)
+  threadDelay micros
+
+send :: TChan [ByteString] -> IO ()
+send i = do chunks <- atomically $ readTChan i
+            mapM_ ByteString.putStr chunks
+            hFlush stdout
+
+
+writeNonEmpty :: TChan [t] -> [t] -> STM ()
+writeNonEmpty chan x = not (null x) `when` writeTChan chan x
+
+readAll :: TChan t -> STM [t]
+readAll chan = do h <- tryReadTChan chan
                   case h of Just h  -> (h:) <$> readAll chan
                             Nothing -> return []
 
-breakIntoRecords :: STM.TChan ByteString
-                 -> STM.TChan (Vector ByteString) -> STM () 
-breakIntoRecords i o = do
-  records <- Vector.fromList . ByteString.lines <$> STM.readTChan i
-  STM.writeTChan o records
 
-getBlock :: STM.TChan ByteString -> IO ()
-getBlock o = do
-  bytes <- ByteString.hGet stdin (16)
-  hPutStrLn stderr "Got a block."
-  when (bytes /= "") $ STM.atomically (STM.writeTChan o bytes)
+msg :: ByteString -> IO ()
+msg  = ByteString.hPutStrLn stderr
 
-send :: STM.TChan [Vector ByteString] -> IO ()
-send i = do
-  vecs <- STM.atomically $ STM.readTChan i
-  let lists = (Vector.toList <$> vecs)
-  mapM_ (mapM_ (ByteString.hPutStrLn stdout)) lists
-  hFlush stdout
+maybeList :: ByteString -> [ByteString] -> IO ()
+maybeList _      [   ] = return ()
+maybeList remark stuff = msg remark >> mapM_ msg stuff >> hFlush stderr
+
+forget :: IO () -> IO ()
+forget io = () <$ forkIO io
+
+
+-- | Stateful CSV recognizer. Takes sequences of whole records from the input
+--   'ByteString', skipping bad records, and returns any remainder.
+segment :: ByteString -> ([Either ByteString ByteString], ByteString)
+segment "" = ([], "")
+segment b  = case step b of (Just res, tail) -> first (res:) $ segment tail 
+                            (Nothing,  tail) -> ([], ByteString.copy tail)
+
+-- | Skips over blank lines from the beginning of the string and, if a full
+--   record (or a bad one, terminated by a newline) is available, parses it
+--   out and returns it with the tail. If not, returns the input string.
+step :: ByteString -> (Maybe (Either ByteString ByteString), ByteString)
+step (ByteString.splitAt 1 -> ("\n",  t)) = step t
+step (ByteString.splitAt 2 -> ("\r\n",t)) = step t
+step b = case plain 0 b of Nothing                  -> (Nothing, b)
+                           Just (n, ok) | ok        -> (Just (Right h), t)
+                                        | otherwise -> (Just (Left h),  t)
+                             where (h, t) = ByteString.splitAt n b
+
+plain, quoted :: Int -> ByteString -> Maybe (Int, Bool)
+
+plain  offset (ByteString.splitAt 1 -> ("\"",  t)) = quoted (offset+1) t
+plain  offset (ByteString.splitAt 2 -> ("\r\n",_)) = Just   (offset+2, True)
+plain  offset (ByteString.splitAt 1 -> ("\n",  _)) = Just   (offset+1, True)
+plain  _      (ByteString.splitAt 1 -> ("\r", "")) = Nothing
+plain  offset (ByteString.splitAt 1 -> ("\r",  _)) = Just   (offset+1, False)
+plain  _      (ByteString.splitAt 1 -> (""  ,  _)) = Nothing
+plain  offset (ByteString.splitAt 1 -> (_   ,  t)) = plain  (offset+1) t
+
+quoted offset (ByteString.splitAt 2 -> ("\"\"",t)) = quoted (offset+2) t
+quoted offset (ByteString.splitAt 1 -> ("\"",  t)) = plain  (offset+1) t
+quoted _      (ByteString.splitAt 1 -> (""  ,  _)) = Nothing
+quoted offset (ByteString.splitAt 1 -> (_   ,  t)) = quoted (offset+1) t
 
